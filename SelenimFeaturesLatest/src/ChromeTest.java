b53c99f4031347766767b7d9d30e7ff6235381d1
Safwan <Shaikh.safwan35@gmail.com>
2025-07-09 23:37:59 +0530

    CDP Changes

---
+++ SelenimFeaturesLatest/src/ChromeTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeTest {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\Browser Drivers\\Chrome Drivers\\chromedriver-win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println("Title: " + driver.getTitle());
+		driver.quit();
+	}
+}
